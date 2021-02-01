c2639be047cba0236d32f20f621957332ff1f8a9
rich-murphey <rich@murphey.org>
2021-02-01 10:16:55 -0600

    simplify attributes, clarify naming

--- src/bytestream.rs
+++ src/bytestream.rs
@@ -36,17 +36,9 @@ impl<T, St> ByteStream<T, St>
 where
     St: Stream<Item = Result<T, sqlx::Error>>,
 {
+    #![allow(dead_code)]
     const DEFAULT_BUF_SIZE: usize = 4096; 
 
-    #[allow(dead_code)]
-    #[inline]
-    pub fn pin<F>(inner: St, f: F) -> Self
-    where
-        F: 'static + FnMut(&mut BytesWriter, &T) -> Result<(), actix_web::Error>,
-    {
-        Self::new(Box::pin(inner), Box::new(f))
-    }
-    #[inline]
     pub fn new(
         inner: Pin<Box<St>>,
         f: Box<dyn FnMut(&mut BytesWriter, &T) -> Result<(), actix_web::Error>>,
@@ -64,31 +56,28 @@ where
             f,
         }
     }
-    #[allow(dead_code)]
-    #[inline]
+    pub fn pin<F>(inner: St, f: F) -> Self
+    where
+        F: 'static + FnMut(&mut BytesWriter, &T) -> Result<(), actix_web::Error>,
+    {
+        Self::new(Box::pin(inner), Box::new(f))
+    }
     pub fn prefix<S: ToString>(mut self, s: S) -> Self {
         self.prefix = s.to_string().into_bytes();
         self
     }
-    #[allow(dead_code)]
-    #[inline]
     pub fn separator<S: ToString>(mut self, s: S) -> Self {
         self.separator = s.to_string().into_bytes();
         self
     }
-    #[allow(dead_code)]
-    #[inline]
     pub fn suffix<S: ToString>(mut self, s: S) -> Self {
         self.suffix = s.to_string().into_bytes();
         self
     }
-    #[allow(dead_code)]
-    #[inline]
     pub fn size(mut self, size: usize) -> Self {
         self.buf_size = size;
         self
     }
-    #[allow(dead_code)]
     pub fn json_array(inner: St) -> Self
     where
         T: serde::Serialize,
@@ -97,28 +86,23 @@ where
             serde_json::to_writer(buf, t).map_err(ErrorInternalServerError)
         })
     }
-    #[inline]
     fn put_prefix(&mut self) {
         self.buf.0.extend_from_slice(&self.prefix);
     }
-    #[inline]
     fn put_separator(&mut self) {
         self.buf.0.extend_from_slice(&self.separator);
     }
-    #[inline]
     fn put_suffix(&mut self) {
         self.buf.0.extend_from_slice(&self.suffix);
     }
-    #[inline]
     fn get_bytes(&mut self) -> Bytes {
         self.buf.0.split().freeze()
     }
-    #[inline]
     fn reserve(&mut self) {
         self.buf.0.reserve(self.buf_size);
     }
-    #[inline]
-    fn set_item_size(&mut self, item_size: usize) {
+    fn adjust_item_size(&mut self, inital_len: usize) {
+        let item_size = self.buf.0.len() - inital_len;
         if self.item_size < item_size {
             self.item_size = item_size;
             while self.buf_size < self.item_size * 5 / 4 {
@@ -126,11 +110,9 @@ where
             }
         }
     }
-    #[inline]
-    fn capacity(&self) {
-        self.buf.0.capacity() - self.buf.0.len()
+    fn has_room_for_item(&self) -> bool {
+        self.item_size <= self.buf.0.capacity() - self.buf.0.len()
     }
-    #[inline]
     fn write_record(&mut self, record: &T) -> Result<(), actix_web::Error> {
         (self.f)(&mut self.buf, record)
     }
@@ -165,11 +147,11 @@ where
                     if let Err(e) = self.write_record(&record) {
                         return Ready(Some(Err(ErrorInternalServerError(e))));
                     }
-                    self.set_item_size(self.buf.0.len() - initial_len);
-                    if self.capacity() < self.item_size {
-                        return Ready(Some(Ok(self.get_bytes())));
-                    } else {
+                    self.adjust_item_size(initial_len);
+                    if self.has_room_for_item() {
                         continue;
+                    } else {
+                        return Ready(Some(Ok(self.get_bytes())));
                     }
                 }
                 Ready(Some(Err(e))) => return Ready(Some(Err(ErrorInternalServerError(e)))),
@@ -193,12 +175,10 @@ where
 pub struct BytesWriter(pub BytesMut);
 
 impl std::io::Write for BytesWriter {
-    #[inline]
     fn write(&mut self, src: &[u8]) -> std::io::Result<usize> {
         self.0.extend_from_slice(src);
         Ok(src.len())
     }
-    #[inline]
     fn flush(&mut self) -> std::io::Result<()> {
         Ok(())
     }
