bee58be57f9213ff30edce44417557d0a49db208
rich-murphey <rich@murphey.org>
2021-02-01 22:02:26 -0600

    add descriptions to methods.

--- src/bytestream.rs
+++ src/bytestream.rs
@@ -59,50 +59,54 @@ where
             f,
         }
     }
-    /// box the stream and serializer function.
+    /// pin and box the stream and box the serializer function.
     pub fn pin(inner: St, f: F) -> Self
     {
         Self::new(Box::pin(inner), Box::new(f))
     }
+    /// set the prefix for the json array. '[' by default.
     pub fn prefix<S: ToString>(mut self, s: S) -> Self {
         self.prefix = s.to_string().into_bytes();
         self
     }
+    /// set the separator for the json array elements. ',' by default.
     pub fn separator<S: ToString>(mut self, s: S) -> Self {
         self.separator = s.to_string().into_bytes();
         self
     }
+    /// set the suffix for the json array. ']' by default.
     pub fn suffix<S: ToString>(mut self, s: S) -> Self {
         self.suffix = s.to_string().into_bytes();
         self
     }
+    /// set the buffer size for the json text.
     pub fn size(mut self, size: usize) -> Self {
         self.buf_size = size;
         self
     }
-    /// append the configured prefix to the output buffer.
+    // append the configured prefix to the output buffer.
     fn put_prefix(&mut self) {
         self.buf.0.extend_from_slice(&self.prefix);
     }
-    /// append the configured separator to the output buffer.
+    // append the configured separator to the output buffer.
     fn put_separator(&mut self) {
         self.buf.0.extend_from_slice(&self.separator);
     }
-    /// append the configured suffix to the output buffer.
+    // append the configured suffix to the output buffer.
     fn put_suffix(&mut self) {
         self.buf.0.extend_from_slice(&self.suffix);
     }
-    /// return the buffered output bytes.
+    // return the buffered output bytes.
     fn get_bytes(&mut self) -> Bytes {
         self.buf.0.split().freeze()
     }
-    /// ensure capacity for at least one additional item to be
-    /// inserted into the buffer.
+    // ensure capacity for at least one additional item to be
+    // inserted into the buffer.
     fn reserve(&mut self) {
         self.buf.0.reserve(self.buf_size);
     }
-    /// Make the buffer 20% larger than the largest item, and round it
-    /// up to a power of two.
+    // Make the buffer 20% larger than the largest item, and round it
+    // up to a power of two.
     fn adjust_item_size(&mut self, inital_len: usize) {
         let item_size = self.buf.0.len() - inital_len;
         if self.item_size < item_size {
@@ -112,12 +116,12 @@ where
             }
         }
     }
-    /// return true if there is room for one more item in the buffer.
+    // return true if there is room for one more item in the buffer.
     fn has_room_for_item(&self) -> bool {
         let remaining_space = self.buf.0.capacity() - self.buf.0.len();
         self.item_size <= remaining_space
     }
-    /// use the given closure to write a record to the output buffer.
+    // use the given closure to write a record to the output buffer.
     fn write_record(&mut self, record: &T) -> Result<(), actix_web::Error> {
         (self.f)(&mut self.buf, record)
     }
