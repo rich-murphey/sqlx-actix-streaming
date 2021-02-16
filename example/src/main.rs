ef3ac75b47ba10f578595cf5f6a1018f9cb84ed2
rich-murphey <rich@murphey.org>
2021-02-16 03:52:27 -0600

    avoid double Arc.

--- example/src/main.rs
+++ example/src/main.rs
@@ -13,12 +13,13 @@ async fn main() -> anyhow::Result<()> {
     dotenv::dotenv().ok();
     env_logger::init();
     let pool = DbPool::connect(&env::var("DATABASE_URL").context("DATABASE_URL")?).await?;
+    let pool = web::Data::new(pool); // avoid double Arc.
     let addr = env::var("SOCKETADDR").unwrap_or_else(|_| "127.0.0.1:8080".to_string());
     info!("this web server is listening at http://{}", &addr);
     HttpServer::new(move || {
         actix_web::App::new()
             .wrap(middleware::Logger::default())
-            .app_data(web::Data::new(pool.clone()))
+            .app_data(pool.clone())
             .configure(widgets::service)
             .default_service(web::route().to(HttpResponse::NotFound))
     })
